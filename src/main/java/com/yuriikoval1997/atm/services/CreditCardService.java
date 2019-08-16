f240224d83016e0c37522a08eda40f45150cf51b
Yurii <10a014k@gmail.com>
2019-08-16 12:43:05 +0300

    Fixed mistake

--- src/main/java/com/yuriikoval1997/atm/services/CreditCardService.java
+++ src/main/java/com/yuriikoval1997/atm/services/CreditCardService.java
@@ -1,9 +1,11 @@
 package com.yuriikoval1997.atm.services;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public interface CreditCardService {
-    int ONE_HUNDRED = 100;
-    int TWO_HUNDRED = 200;
-    int FIVE_HUNDRED = 500;
+    List<Integer> BANKNOTES = Collections.unmodifiableList(Arrays.asList(100, 200, 500));
 
     boolean isAppropriateAmount(long amount);
 }
--- src/main/java/com/yuriikoval1997/atm/services/CreditCardServiceImpl.java
+++ src/main/java/com/yuriikoval1997/atm/services/CreditCardServiceImpl.java
@@ -3,5 +3,12 @@ package com.yuriikoval1997.atm.services;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CreditCardServiceImpl {
+public class CreditCardServiceImpl implements CreditCardService{
+
+    @Override
+    public boolean isAppropriateAmount(long amount) {
+        return BANKNOTES
+                .stream()
+                .anyMatch(note -> amount % note == 0);
+    }
 }
